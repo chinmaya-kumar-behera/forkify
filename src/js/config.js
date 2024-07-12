c54f201e11c665ade9f313867de2d3e86594b8de
chinmaya-kumar-behera <official.chinu2127@gmail.com>
2024-07-13 03:55:37 +0530

    add: Api Key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'c6e1e2ee-3ffc-4742-89cc-5b3d5eb9bd40';
 export const MODAL_CLOSE_SEC = 2.5;
